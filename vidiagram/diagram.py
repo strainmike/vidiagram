4c85e194e116511d3eb332e426bfcfc3157b626a
Michael Strain <strainmike@gmail.com>
2022-08-31 16:54:16 -0400

    Rewrote how Weakreferences get resolved

--- vidiagram/diagram.py
+++ vidiagram/diagram.py
@@ -7,74 +7,78 @@ import graphviz
 
 this_dir = os.path.dirname(os.path.realpath(__file__))
 
-uuid_dict = {}
-
 from pylabview import LVheap
 def get_class_type(node):
     if LVheap.SL_SYSTEM_ATTRIB_TAGS.SL__class.value in node.attribs:
         return node.attribs[LVheap.SL_SYSTEM_ATTRIB_TAGS.SL__class.value]
     return None
 
-def node_to_object(node):
+def node_to_object(uuid_manager, node):
     class_type = get_class_type(node)
-    subclass = node.findChild(LVheap.OBJ_FIELD_TAGS.OF__dco)
-    if subclass:
-        subclass_node = node_to_object_and_uuid_dict(subclass)
-        subclass_type = get_class_type(subclass)
-
     # print(class_type)
-    # print(subclass)
     if class_type is None and LVheap.SL_SYSTEM_ATTRIB_TAGS.SL__uid.value in node.attribs:
         return WeakNode(node)
     if class_type == LVheap.SL_CLASS_TAGS.SL__diag:
-        return Diagram(node)
+        return Diagram(uuid_manager, node)
     if class_type == LVheap.SL_CLASS_TAGS.SL__whileLoop:
-        return WhileLoop(node)
+        return WhileLoop(uuid_manager, node)
     if class_type == LVheap.SL_CLASS_TAGS.SL__forLoop:
-        return WhileLoop(node)
+        return WhileLoop(uuid_manager, node)
     if class_type == LVheap.SL_CLASS_TAGS.SL__prim:
-        return Primitive(node)
+        return Primitive(uuid_manager, node)
     if class_type == LVheap.SL_CLASS_TAGS.SL__bDConstDCO:
-        return Constant(node)
+        return Constant(uuid_manager, node)
     if class_type == LVheap.SL_CLASS_TAGS.SL__fPTerm:
-        return FPTerminal(node)
+        return FPTerminal(uuid_manager, node)
     if class_type == LVheap.SL_CLASS_TAGS.SL__term:
-        # if subclass:
-            # if subclass_type == LVheap.SL_CLASS_TAGS.SL__parm:
-            # if subclass_type == LVheap.SL_CLASS_TAGS.SL__overridableParm:
-            # if subclass_type == LVheap.SL_CLASS_TAGS.SL__lpTun:
-        return Terminal(node)
+        return Terminal(uuid_manager, node)
     if class_type == LVheap.SL_CLASS_TAGS.SL__lpTun:
-        return LoopTunnel(node)
+        return LoopTunnel(uuid_manager, node)
     if class_type == LVheap.SL_CLASS_TAGS.SL__parm:
-        return Parameter(node)
+        return Parameter(uuid_manager, node)
     if class_type == LVheap.SL_CLASS_TAGS.SL__overridableParm:
-        return Output(node)
+        return Output(uuid_manager, node)
     if class_type == LVheap.SL_CLASS_TAGS.SL__bDConstDCO:
-        return Constant(node)
+        return Constant(uuid_manager, node)
     if class_type == LVheap.SL_CLASS_TAGS.SL__lTst:
-        return LoopTest(node)
+        return LoopTest(uuid_manager, node)
     if class_type == LVheap.SL_CLASS_TAGS.SL__lCnt:
-        return LoopCount(node)
+        return LoopCount(uuid_manager, node)
     if class_type == LVheap.SL_CLASS_TAGS.SL__label:
-        return Label(node)
+        return Label(uuid_manager, node)
 
     else:
         # raise node
-        return UnknownNode(node)
+        return UnknownNode(uuid_manager, node)
 
-def node_to_object_and_uuid_dict(node):
-    if node.getScopeInfo() is LVheap.NODE_SCOPE.TagClose:
-        return
-    if LVheap.SL_SYSTEM_ATTRIB_TAGS.SL__uid.value in node.attribs:
-        uuid = node.attribs[LVheap.SL_SYSTEM_ATTRIB_TAGS.SL__uid.value]
-        # print(uuid)
-        if uuid in uuid_dict:
-            return uuid_dict[uuid]
-    graph_node = node_to_object(node)
-    if not isinstance(graph_node, WeakNode) and LVheap.SL_SYSTEM_ATTRIB_TAGS.SL__uid.value in node.attribs:
-        uuid_dict[uuid] = graph_node
-    return graph_node
+class UUIDManager:
+    def __init__(self, uuids_to_nodes):
+        self._uuids_to_nodes = uuids_to_nodes
+        self._uuids_to_obj = {}
+        for uuid, node in uuids_to_nodes.items():
+            self._uuids_to_obj[uuid] = self.node_to_object(node)
+        for uuid, node_obj in self._uuids_to_obj.items():
+            node_obj.resolve_weak_nodes()
+
+    def node_to_object(self, node):
+        if node.getScopeInfo() is LVheap.NODE_SCOPE.TagClose:
+            return
+        if LVheap.SL_SYSTEM_ATTRIB_TAGS.SL__uid.value in node.attribs:
+            uuid = node.attribs[LVheap.SL_SYSTEM_ATTRIB_TAGS.SL__uid.value]
+            # print(uuid)
+            if uuid in self._uuids_to_obj:
+                return self._uuids_to_obj[uuid]
+            else:
+                print(uuid)
+                return node_to_object(self, self._uuids_to_nodes[uuid])
+
+        graph_node = node_to_object(self, node)
+        if not isinstance(graph_node, WeakNode) and LVheap.SL_SYSTEM_ATTRIB_TAGS.SL__uid.value in node.attribs:
+            self._uuids_to_obj[uuid] = graph_node
+        return graph_node
+
+    def lookup_node_object(self, uuid):
+        return self._uuids_to_obj[uuid]
 
 
 class WeakNode:
@@ -89,12 +93,13 @@ class WeakNode:
     __repr__ = __str__
 
 class Signal:
-    def __init__(self, terminal_list):
+    def __init__(self, uuid_manager, terminal_list):
         self._terminals = []
         for terminal in iterate_direct_children(terminal_list):
             uuid = terminal.attribs[LVheap.SL_SYSTEM_ATTRIB_TAGS.SL__uid.value]
             self._terminals.append(uuid)
-            node_to_object_and_uuid_dict(terminal) # in case any actual objects show up here instead of just references, resolve them, not sure if this is possible
+            self.uuid_manager = uuid_manager
+            uuid_manager.node_to_object(terminal) # in case any actual objects show up here instead of just references, resolve them, not sure if this is possible
 
     def fill_graph(self, graph, namespace):
         if not self._terminals or len(self._terminals) == 1:
@@ -102,22 +107,28 @@ class Signal:
         iterator = iter(self._terminals)
         source = next(iterator)
         for dest in iterator:
-            graph.edge(uuid_dict[source].name, uuid_dict[dest].name)
+            graph.edge(self.uuid_manager.lookup_node_object(source).name, self.uuid_manager.lookup_node_object(dest).name)
 
 
 class Node:
-    def __init__(self, node):
+    def __init__(self, uuid_manager, node):
         self._node = node
         self._uuid = node.attribs[LVheap.SL_SYSTEM_ATTRIB_TAGS.SL__uid.value]
         self._terminals = []
         self._resolved_weaknodes = False
         self._color = None
         self._style = None
+        self.uuid_manager = uuid_manager
+
+        self._impl = None
+        subclass = node.findChild(LVheap.OBJ_FIELD_TAGS.OF__dco)
+        if subclass:
+            self._impl = WeakNode(subclass)
 
         terminal_list = node.findChild(LVheap.OBJ_FIELD_TAGS.OF__termList)
         for terminal in iterate_direct_children(terminal_list):
             # print("Parsing termlist: " + str(terminal))
-            self._terminals.append(node_to_object_and_uuid_dict(terminal))
+            self._terminals.append(WeakNode(terminal))
         bounds = node.findChild(LVheap.OBJ_FIELD_TAGS.OF__bounds)
 
         if bounds:
@@ -127,15 +138,21 @@ class Node:
     def resolve_weak_nodes(self):
         if self._resolved_weaknodes:
             return
+        self._resolved_weaknodes = True
         print("resolve: " + str(self))
         for index, terminal in enumerate(self._terminals):
             if isinstance(terminal, WeakNode):
                 print("resolving: " + str(terminal))
-                self._terminals[index] = node_to_object_and_uuid_dict(terminal._node)
+                self._terminals[index] = self.uuid_manager.node_to_object(terminal._node)
         for terminal in self._terminals:
             terminal.resolve_weak_nodes()
             terminal.set_parent(self)
-        self._resolved_weaknodes = True
+        if self._impl:
+            if isinstance(self._impl, WeakNode):
+                self._impl = self.uuid_manager.node_to_object(self._impl._node)
+            if not isinstance(self._impl, WeakNode):
+                self._impl.resolve_weak_nodes()
+                self._impl.set_parent(self)
 
     @property
     def name(self):
@@ -152,19 +169,19 @@ class Node:
         # if no or 1 terminal, maybe just make it a node
         if len(self._terminals) > 0:
             with graph.subgraph(name=self.name) as subgraph:
-                subgraph.attr(label=self.label, color=self._color, style="filled")
+                subgraph.attr(label=self.label, fillcolor=self._color, style="filled,solid")
                 for terminal in self._terminals:
                 # name = f"<{str(terminal._uuid)}> Terminal"
                 # struct_info += name + " | "
                     print(terminal)
                     terminal.fill_graph(subgraph, namespace)
         else:
-            graph.node(name=self.name, label=self.label, color=self._color, style=self._style)
+            graph.node(name=self.name, label=self.label, fillcolor=self._color, style=self._style)
 
 
 class UnknownNode(Node):
-    def __init__(self, node):
-        super().__init__(node)
+    def __init__(self, uuid_manager, node):
+        super().__init__(uuid_manager, node)
         print(f"Unknown {get_class_type(node)}")
 
     def __str__(self):
@@ -177,22 +194,24 @@ class UnknownNode(Node):
 
 
 class Label(Node):
-    def __init__(self, node):
-        super().__init__(node)
+    def __init__(self, uuid_manager, node):
+        super().__init__(uuid_manager, node)
         textrec = node.findChild(LVheap.OBJ_FIELD_TAGS.OF__textRec)
         print(textrec)
+        print(self._uuid)
         text = textrec.findChild(LVheap.OBJ_TEXT_HAIR_TAGS.OF__text)
-        self._label = text.content.decode("cp1252")
+        if text:
+            self._label = text.content.decode("cp1252")
         self._color = "lightyellow"
-        self._style = "filled"
+        self._style = "filled,solid"
 
     @property
     def label(self):
         return self._label
 
 class FPTerminal(Node):
-    def __init__(self, node):
-        super().__init__(node)
+    def __init__(self, uuid_manager, node):
+        super().__init__(uuid_manager, node)
 
     def __str__(self):
         return "FPTerminal"
@@ -203,22 +222,9 @@ class FPTerminal(Node):
     __repr__ = __str__
 
 class Terminal(Node):
-    def __init__(self, node):
-        super().__init__(node)
-        subclass = node.findChild(LVheap.OBJ_FIELD_TAGS.OF__dco)
-        # print("Terminal: "+str(self._uuid))
-        self._impl = node_to_object_and_uuid_dict(subclass)
+    def __init__(self, uuid_manager, node):
+        super().__init__(uuid_manager, node)
         self._parent = None
-
-    def resolve_weak_nodes(self):
-        if self._resolved_weaknodes:
-            return
-        # print("resolve terminals")
-        super().resolve_weak_nodes()
-        if isinstance(self._impl, WeakNode):
-            self._impl = node_to_object_and_uuid_dict(self._impl._node)
-        self._impl.resolve_weak_nodes() # TODO why does this recurse forever
-        self._impl.set_parent(self)
 
     def set_parent(self, parent):
         # if self._parent and self._parent is not parent:
@@ -245,8 +251,8 @@ class Terminal(Node):
     __repr__ = __str__
 
 class LoopTunnel(Node):
-    def __init__(self, node):
-        super().__init__(node)
+    def __init__(self, uuid_manager, node):
+        super().__init__(uuid_manager, node)
 
     def set_parent(self, parent):
         self._parent = parent
@@ -281,8 +287,8 @@ class LoopTunnel(Node):
 
 
 class TerminalClass(Node):
-    def __init__(self, node):
-        super().__init__(node)
+    def __init__(self, uuid_manager, node):
+        super().__init__(uuid_manager, node)
 
     def fill_graph(self, graph, namespace):
         graph.node(name=self._parent.name, label=type(self).__name__)
@@ -291,20 +297,20 @@ class TerminalClass(Node):
         self._parent = parent
 
 class Constant(TerminalClass):
-    def __init__(self, node):
-        super().__init__(node)
+    def __init__(self, uuid_manager, node):
+        super().__init__(uuid_manager, node)
 
 class Parameter(TerminalClass):
-    def __init__(self, node):
-        super().__init__(node)
+    def __init__(self, uuid_manager, node):
+        super().__init__(uuid_manager, node)
 
 class Output(TerminalClass):
-    def __init__(self, node):
-        super().__init__(node)
+    def __init__(self, uuid_manager, node):
+        super().__init__(uuid_manager, node)
 
 class Primitive(Node):
-    def __init__(self, node):
-        super().__init__(node)
+    def __init__(self, uuid_manager, node):
+        super().__init__(uuid_manager, node)
         self._primitive_id = node.attribs[LVheap.SL_SYSTEM_TAGS.SL__object.value]
         self._color = "lightyellow"
 
@@ -314,14 +320,14 @@ class Primitive(Node):
     __repr__ = __str__
 
 class Structure(Node):
-    def __init__(self, node):
-        super().__init__(node)
+    def __init__(self, uuid_manager, node):
+        super().__init__(uuid_manager, node)
         diagram_list = node.findChild(LVheap.OBJ_FIELD_TAGS.OF__diagramList)
         diagram_list_size = diagram_list.attribs[LVheap.SL_SYSTEM_ATTRIB_TAGS.SL__elements.value]
         self._diagrams = []
         for diagram_index in range(diagram_list_size):
             raw_diagram = diagram_list.findChild(LVheap.SL_SYSTEM_TAGS.SL__arrayElement, diagram_index)
-            self._diagrams.append(node_to_object_and_uuid_dict(raw_diagram))
+            self._diagrams.append(uuid_manager.node_to_object(raw_diagram))
 
     def fill_graph(self, graph, namespace):
         with graph.subgraph(name="cluster" + str(self._uuid)) as subgraph:
@@ -345,19 +351,19 @@ class Structure(Node):
 
 
 class LoopTest(TerminalClass):
-    def __init__(self, node):
-        super().__init__(node)
+    def __init__(self, uuid_manager, node):
+        super().__init__(uuid_manager, node)
 
 class LoopCount(TerminalClass):
-    def __init__(self, node):
-        super().__init__(node)
+    def __init__(self, uuid_manager, node):
+        super().__init__(uuid_manager, node)
 
 
 class Loop(Structure):
-    def __init__(self, node):
-        super().__init__(node)
-        self._loopIndex = node_to_object_and_uuid_dict(node.findChild(LVheap.OBJ_FIELD_TAGS.OF__loopIndexDCO))
-        self._loopTest = node_to_object_and_uuid_dict(node.findChild(LVheap.OBJ_FIELD_TAGS.OF__loopTestDCO))
+    def __init__(self, uuid_manager, node):
+        super().__init__(uuid_manager, node)
+        self._loopIndex = uuid_manager.node_to_object(node.findChild(LVheap.OBJ_FIELD_TAGS.OF__loopIndexDCO))
+        self._loopTest = uuid_manager.node_to_object(node.findChild(LVheap.OBJ_FIELD_TAGS.OF__loopTestDCO))
 
 
 
@@ -373,8 +379,8 @@ def iterate_direct_children(node):
             yield child
 
 class Diagram(Node):
-    def __init__(self, node):
-        super().__init__(node)
+    def __init__(self, uuid_manager, node):
+        super().__init__(uuid_manager, node)
         self._nodes = []
         self._terminals = []
         self._signals = []
@@ -384,25 +390,25 @@ class Diagram(Node):
             if get_class_type(nodelist_item) == LVheap.SL_CLASS_TAGS.SL__sRN:
                 terminals = nodelist_item.findChild(LVheap.OBJ_FIELD_TAGS.OF__termList)
                 for terminal in iterate_direct_children(terminals):
-                    subnode = node_to_object_and_uuid_dict(terminal)
+                    subnode = WeakNode(terminal)
                     if subnode:
                         self._terminals.append(subnode)
 
         zPlaneList = node.findChild(LVheap.OBJ_FIELD_TAGS.OF__zPlaneList)
         for child_node in zPlaneList.childs:
-            subnode = node_to_object_and_uuid_dict(child_node)
+            subnode = self.uuid_manager.node_to_object(child_node)
             if subnode:
                 self._nodes.append(subnode)
         signallist = node.findChild(LVheap.OBJ_FIELD_TAGS.OF__signalList)
         for signal in iterate_direct_children(signallist):
             connected_terminals = signal.findChild(LVheap.OBJ_FIELD_TAGS.OF__termList)
-            self._signals.append(Signal(connected_terminals))
+            self._signals.append(Signal(self.uuid_manager, connected_terminals))
 
     def resolve_weak_nodes(self):
         super().resolve_weak_nodes()
         for index, node in enumerate(self._nodes):
             if isinstance(node, WeakNode):
-                self._nodes[index] = node_to_object_and_uuid_dict(node._node)
+                self._nodes[index] = self.uuid_manager.node_to_object(node._node)
         for node in self._nodes:
             node.resolve_weak_nodes()
 
@@ -421,43 +427,44 @@ class Diagram(Node):
 
     __repr__ = __str__
 
+def create_uuid_dict(heap):
+    local_dict = {}
+    for node in iterate_direct_children(heap):
+        print(node)
+        if get_class_type(node) != None and LVheap.SL_SYSTEM_ATTRIB_TAGS.SL__uid.value in node.attribs:
+            uuid = node.attribs[LVheap.SL_SYSTEM_ATTRIB_TAGS.SL__uid.value]
+            local_dict[uuid] = node
+        local_dict.update(create_uuid_dict(node))
+    return local_dict
 
-
-class TestDiagram(unittest.TestCase):
-    def test_parse_diagram(self):
-        vi_obj = vi.get_vi(os.path.join(this_dir, "add.vi"))
-        for block in vi_obj.blocks.values():
-            block_identifier = bytearray(block.header.ident).decode()
-            if 'BDH' in block_identifier:
-                # print(block.sections)
-                for index, section in block.sections.items():
-                    root = section.objects[0].childs[0]
-                    diagram = node_to_object_and_uuid_dict(root)
-                    diagram.resolve_weak_nodes()
-                    # print(diagram)
-                    # pprint(uuid_dict)
-                    g = graphviz.Digraph('G', engine='dot', node_attr={"shape": "record"})
-                    diagram.fill_graph(g)
-                    # g.view()
-                    # print(g.source)
-            # breakpoint()
 
 def get_dot_graph(file):
     vi_obj = vi.get_vi(file)
+    uuids_to_nodes = {}
+    block_diagram_root = None
     for block in vi_obj.blocks.values():
         block_identifier = bytearray(block.header.ident).decode()
         if 'BDH' in block_identifier:
             # print(block.sections)
             for index, section in block.sections.items():
+                uuids_to_nodes.update(create_uuid_dict(section.objects[0]))
                 root = section.objects[0].childs[0]
-                diagram = node_to_object_and_uuid_dict(root)
-                diagram.resolve_weak_nodes()
-                print(diagram)
-                pprint(uuid_dict)
-                g = graphviz.Digraph('G', engine='dot', format="svg", node_attr={"shape": "record"})
-                diagram.fill_graph(g)
-                print(g.source)
-                return g
+                block_diagram_root = root
+        if "FPH" in block_identifier:
+            for index, section in block.sections.items():
+                uuids_to_nodes.update(create_uuid_dict(section.objects[0]))
+                root = section.objects[0].childs[0]
+
+    pprint(uuids_to_nodes)
+    uuid_manager = UUIDManager(uuids_to_nodes)
+    diagram = uuid_manager.node_to_object(block_diagram_root)
+    # diagram.resolve_weak_nodes()
+    print(diagram)
+    g = graphviz.Digraph('G', engine='dot', format="svg", node_attr={"shape": "record"})
+    diagram.fill_graph(g)
+    pprint(uuid_manager._uuids_to_obj)
+    print(g.source)
+    return g
 
 import argparse
 
